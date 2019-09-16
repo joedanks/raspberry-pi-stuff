853ee5c6e600c3b31162581484022cbaa7a951a3
joedanks <joedanks@gmail.com>
2019-09-15 22:10:33 -0500

    Setting up most of waypoint logic

--- gps_calc.py
+++ gps_calc.py
@@ -1,7 +1,7 @@
 import utm
 import math
 
-def calcBearingAndDist(current, waypoint):
+def calcDistAndBearing(current, waypoint):
     (aX, aY, aNum, aLet) = utm.from_latlon(current[0], current[1])
     (bX, bY, bNum, bLet) = utm.from_latlon(waypoint[0], waypoint[1])
 
@@ -17,3 +17,37 @@ def calcBearingAndDist(current, waypoint):
     bearingDeg = math.degrees(bearingRads)
 
     return (z, bearingDeg)
+
+def computeDirectionPixel (degrees):
+    if degrees >= 342 or degrees < 18:
+        return [7]
+    if 324 <= degrees < 342:
+        return [7,8]
+    if 306 <= degrees < 324:
+        return [8]
+    if 288 <= degrees < 306:
+        return [8,9]
+    if 252 <= degrees < 288:
+        return [9,0]
+    if 234 <= degrees < 252:
+        return [0,1]
+    if 216 <= degrees < 234:
+        return [1]
+    if 198 <= degrees < 216:
+        return [1,2]
+    if 162 <= degrees < 198:
+        return [2]
+    if 144 <= degrees < 162:
+        return [2,3]
+    if 126 <= degrees < 144:
+        return [3]
+    if 108 <= degrees < 126:
+        return [3,4]
+    if 72 <= degrees < 108:
+        return [4,5]
+    if 54 <= degrees < 72:
+        return [5,6]
+    if 36 <= degrees < 54:
+        return [6]
+    if 18 <= degrees < 36:
+        return [6,7]
--- waypoint.py
+++ waypoint.py
@@ -4,8 +4,10 @@ import busio
 import adafruit_lsm303
 import adafruit_gps
 import math
+import gps_calc
+from express import cpx
 
-### GPS
+##### GPS ####################
 #   circuit python
 RX = board.RX
 TX = board.TX
@@ -17,15 +19,56 @@ uart = busio.UART(TX, RX, baudrate=9600, timeout=30)
 gps = adafruit_gps.GPS(uart, debug=False)
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
 gps.send_command(b'PMTK220,1000')
+##### GPS End ################
 
+##### Compass ################
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm303.LSM303(i2c)
+##### Compass End ############
+
+cpx.pixels.brightness = 0.3
 
 last_print = time.monotonic()
 current_fix = False
+
+waypoint = None
+dist = None
+bearing = None
+
+def listen_to_set_waypoint():
+    if cpx.button_a:
+        if gps.has_fix:
+            waypoint = (gps.latitude, gps.longitude)
+            print('Waypoint set to ({0:.6f},{0:.6f})'.format(gps.latitude, gps.longitude))
+        else:
+            print('Unable to set waypoint. No GPS fix.')
+        while cpx.button_a:
+            continue
+
+def turnDistPixelsOn():
+    if dist is not None:
+        cpx.pixels.fill((0, 0, 0))
+        x = math.trunc(dist / 5)
+        for n in range(x, 9):
+            cpx.pixels[n] = (0,255,0)
+
+def turnBearingPixelOn():
+    if bearing is not None:
+        cpx.pixels.fill((0,0,0))
+        for x in gps_calc.computeDirectionPixel(bearing):
+            cpx.pixels[x] = (0,0,255)
+
+def displayPixelsOnSwitch():
+    if cpx.switch:
+        turnBearingPixelOn()
+    else:
+        turnDistPixelsOn()
+
 print('Searching for satellites...')
 while True:
     gps.update()
+
+    listen_to_set_waypoint()
 
     current = time.monotonic()
     if current - last_print >= 1.0:
@@ -36,9 +79,17 @@ while True:
 
         if not gps.has_fix and current_fix:
             print('Lost GPS fix')
+            current_fix = False
+            cpx.pixels.fill((255,0,0))
             continue
         else if gps.has_fix and not current_fix:
             print('GPS fix established')
             current_fix = True
+
+        if current_fix and waypoint is not None:
+            (dist, bearing) = gps_calc.calcDistAndBearing((gps.latitude, gps.longitude), waypoint)
+            displayPixelsOnSwitch()
+
+            
 
 
